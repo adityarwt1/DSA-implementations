fe5954c2cc2fd4de255a61a1813d94780b541f93
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-25 07:49:33 +0530

    trtying cd of number

---
+++ BasicMaths/GcdOfNumber/gcdOfNumber.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    map<int , int> factTorialOfNumber(int number , map<int, int> map){
+        /// base condditon
+        // if(number == 1){
+        //     map.        
+        //     return 
+        // }
+    };
+    int gcdOfNumber(int& num1 , int& num2){
+        // storing the value
+        map<int , int> num1Map;
+        map<int , int> num2Map;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
