fb0b4c3aa8a94b57a076f3084082bdd2ce45a931
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-26 12:37:12 +0530

    560 subarray sum done

--- Leetcode/medium/560SubarraySumEqualsK/subarraysumequaltok.cpp
+++ Leetcode/medium/560SubarraySumEqualsK/subarraysumequaltok.cpp
@@ -7,12 +7,11 @@ public:
         int count = 0;
         for(int i = 0 ; i < nums.size() ; i++){
             int sum = 0 ; 
-            for(int j = i ; j <= nums.size() ; j++){
+            for(int j = i ; j < nums.size() ; j++){
                 sum += nums[j];
 
                 if(sum == k ){
                   count++;
-                  break;
                 }
             }
         }
