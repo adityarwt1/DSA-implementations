f50f35b78d8af3407c6e898730ea2eb36f36601c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-26 17:03:47 +0530

    find minimum in teh

---
+++ Leetcode/medium/153FindMinimuminRotatedSortedArray/findminiuminroatedsortedarray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0 , high = nums.size() -1;
+
+
+        while(low < high){
+
+            int mid = low + (high -low) /2;
+
+            if(nums[mid] > nums[high]){
+                low = mid +1;
+            }
+            else{
+                high  = mid;
+            }
+        }
+
+        return nums[low];
+    }
+};
+int main() {
+    Solution sol;
+    
+    return 0;
+}
