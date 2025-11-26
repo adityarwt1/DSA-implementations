f93d40f73932c726b31ce03256a57bbe78387d1a
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-26 13:05:58 +0530

    made the subarraysumequeal to k

--- Leetcode/medium/560SubarraySumEqualsK/subarraysumequaltok.cpp
+++ Leetcode/medium/560SubarraySumEqualsK/subarraysumequaltok.cpp
@@ -18,7 +18,39 @@ public:
         }
 
         return count;
-    }  
+    } 
+    
+    int optimalAproach(vector<int>& nums , int k ){
+  int n = nums.size();
+    vector<int> prefix(n);
+
+    prefix[0] = nums[0];
+    for(int i = 1; i < n; i++){
+        prefix[i] = prefix[i - 1] + nums[i];
+    }
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+
+            int sum;
+
+            if(i == 0){
+                sum = prefix[j];
+            }
+            else{
+                sum = prefix[j] - prefix[i - 1];
+            }
+
+            if(sum == k){
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
     
     
 };
@@ -36,5 +68,8 @@ int main() {
     vector<int> nums3 = {1,-1,0};
     int result3 = sol.subarraySumBruteforce(nums3 , 0);
     cout << result3 << endl;
+
+    int optimal1 = sol.optimalAproach(nums , 2);
+    cout << optimal1 << endl;
     return 0;
 }
--- Mistake/Mistake.md
+++ Mistake/Mistake.md
@@ -1,2 +1,3 @@
 1. Taking the range nums.size() instead of nums.size() -1.
 2. Just remembering the old answer instead of thinking currently.
+3. In the prefixsum taking thew value from the 0 inseead of the 1, 0 is already included.
