b83c51d86a8b383a26982e1838d4dd9a1f5dbe9c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-27 19:13:49 +0530

    code story with mik is done:

--- DailyChallenges/Novemver/3381MaximumSubarraySumWithLengthDivisiblebyK/codestorywithmik.cpp
+++ DailyChallenges/Novemver/3381MaximumSubarraySumWithLengthDivisiblebyK/codestorywithmik.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Solution {
 public:
-    long long maxSubarraySum(vector<int>& nums, int k) {
+    long long maxSubarraySuBrute(vector<int>& nums, int k) {
         long long ans = LLONG_MIN;
         
         // prefix sum
@@ -26,12 +26,47 @@ public:
 
         return ans;
     }
+
+     long long maxSubarraySum(vector<int>& nums, int k) {
+
+        vector<int> prefixSum(nums.size());
+        prefixSum[0] = nums[0];
+        for(int i = 1  ; i< nums.size() ; i++){
+            prefixSum[i]  = prefixSum[i-1] + nums[i];
+        }
+
+
+        long long result = LLONG_MIN;
+
+        for(int start = 0 ; start < k ; start++){
+                long long  currentSum = 0;
+
+                int i  = start;
+
+            while(i< nums.size() && i + k -1 < nums.size()){
+                    int j = i + k -1;
+                
+                    long long subSum = prefixSum[j] - (i > 0  ? prefixSum[i-1] :0);
+
+                    currentSum = max(subSum , currentSum + subSum);
+
+                    result = max(result, currentSum);
+
+                    i+= k;
+            }
+        }
+
+        return result;
+    }
 };
 
 int main() {
     Solution sol;
     vector<int> nums = {1,2};
-    long long result  = sol.maxSubarraySum(nums , 1);
+    long long result  = sol.maxSubarraySuBrute(nums , 1);
     cout << result << endl;
+    // second try
+    long long restult2 = sol.maxSubarraySum(nums, 1);
+    cout << restult2 << endl;
     return 0;
 }
