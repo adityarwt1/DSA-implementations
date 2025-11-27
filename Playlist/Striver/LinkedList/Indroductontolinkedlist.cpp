24cf46294b63850878fc65df0c7ccbd355b36ee3
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-27 10:17:58 +0530

    indroductroin of linked list is done

--- Playlist/Striver/LinkedList/Indroductontolinkedlist.cpp
+++ Playlist/Striver/LinkedList/Indroductontolinkedlist.cpp
@@ -1,88 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class LinkedList{
-    public:
-        struct Node{
-            int data;
-            Node* next;
-            Node(int val){
-                data = val;
-                next= nullptr;
-            }
-        };
-
-
-        Node* head;
-
-        LinkedList(){
-            head = nullptr;
-        }
-
-        void insert(int val){
-            Node* newNode = new Node(val);
-
-            // if head is empty
-            if(!head){
-                head = newNode;
-                return;
-            }
-
-            // currentnode
-            Node* temp = head;
-
-
-            while(temp->next != nullptr){
-                // linear travel
-                temp = temp->next;
-            }
-
-            temp->next = newNode;
-        }
-
-        void display(){
-            Node* temp = head;
-
-            while(temp != nullptr){
-                cout << temp->data << " -> ";
-                temp = temp->next;
-            }
-            cout << "NULL\n";
-        }
-
-        bool findValue(Node* head , int target){
-
-            if(!head) return false;
-
-            Node* temp = head;
-
-            while(temp != nullptr){
-                if(temp->data == target){
-                    return true;
-                }
-
-                temp = temp->next;
-            }
-
-            return false;
-
-        }
-};
-class Solution {
+class LinkedList {
 public:
+    struct Node {
+        int data;
+        Node* next;
+        Node(int val) {
+            data = val;
+            next = nullptr;
+        }
+    };
 
+    Node* head;
 
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+
+        if (!head) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+
+    bool findValue(int target) {
+        Node* temp = head;
+        while (temp != nullptr) {
+            if (temp->data == target)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
 };
 
 int main() {
-    Solution sol;
     LinkedList list;
 
     list.insert(10);
     list.insert(20);
 
-    
+    bool result = list.findValue(20);
+    if (result) cout << "Value found" << endl;
+
     list.display();
     return 0;
 }
