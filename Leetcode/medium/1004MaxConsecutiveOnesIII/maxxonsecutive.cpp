4dd2c3233077831f93612db91085b34e6824cda5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-28 20:28:34 +0530

    1004 bruter forc eand the optimal is done

--- Leetcode/medium/1004MaxConsecutiveOnesIII/maxxonsecutive.cpp
+++ Leetcode/medium/1004MaxConsecutiveOnesIII/maxxonsecutive.cpp
@@ -7,9 +7,10 @@ public:
 /**
  * intutionc for this
  * i) k time tring for the twopointer
- * 
+ * ii) when found the 0 counting itself becuse the and minuse the operation 
+ * iii) imporve ment needed like start from the where end the last opr thing
  */
-    int longestOnes(vector<int>& nums, int k) {
+    int longestOnesBrute(vector<int>& nums, int k) {
             int maxLenght = 0;
         for(int i = 0 ; i < nums.size() ; i++){
             int opr = k;
@@ -36,6 +37,40 @@ public:
 
         return maxLenght;
     }
+
+    /**
+     * i) taking left as a variable instead of brute force
+     * ii) some observation found the when the 
+     * iii) k se jayda hoga to left plus karenge
+     */
+    int longestOnes(vector<int>& nums , int k){
+        int left = 0 ;
+
+        // window have zero
+        int zeroCount = 0;
+
+        // for the length comparision
+        int maxLength = 0;
+
+
+        for(int right = 0 ; right < nums.size() ; right++){
+            // plush the zero count
+            if(nums[right] == 0)zeroCount++;
+
+            // time to shirk when to much zeroes
+            while(zeroCount > k){
+                // for the longest se samajha karana padata hai
+                if(nums[left] == 0) zeroCount--;
+                left++;
+            }
+
+            // calculating the window sum
+            maxLength = max(maxLength , right - left +1);
+        }
+
+        return maxLength;
+    }
+
 };
 
 int main() {
