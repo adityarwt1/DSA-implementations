ee14ced8f4b887c07c21460554cd1390a91dfc9c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-28 09:07:06 +0530

    bitmanupulation

--- Playlist/Striver/BitManupulation/BitManupulation.cpp
+++ Playlist/Striver/BitManupulation/BitManupulation.cpp
@@ -3,13 +3,46 @@ using namespace std;
 
 class Solution {
 public:
+
+    int reverseNumber(int n ){
+        int ans = 0;
+
+        while(n != 0){
+            int lastNumber = n % 10;
+
+            ans = ans *10 + lastNumber;
+
+            n = n /10;
+        }
+
+        return ans;
+    }
     long long convertToBinary(int n ){
 
         long long ans = 0;
 
         while(n!= 0){
+            // binary is basically moodules of 
             ans = ans * 10 + n % 2;
             n = n /2;
+        }
+
+        return ans;
+    }
+
+    int binaryToNumber(int x){
+        int n = reverseNumber(x);
+        int ans =0 ;
+        int index = 0;
+        while(n != 0){
+            int lastNumber = n % 10 ;
+
+            ans += pow(lastNumber , index);
+
+            index++;
+
+            /// n to 0
+            n = n /10;
         }
 
         return ans;
@@ -24,5 +57,9 @@ int main() {
     int number2  = 13;
     long long result2  = sol.convertToBinary(number2);
     cout << result2 << endl;
+    // binary to number
+    int example1 = 1011;
+    int numberConverted = sol.binaryToNumber(example1);
+    cout << numberConverted << endl;
     return 0;
 }
