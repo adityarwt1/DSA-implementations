869f2208788e2b208dfd6257b5857c0a1225b8e3
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-28 14:00:39 +0530

    stack using linkedlist

--- Lags/Readme.md
+++ Lags/Readme.md
@@ -1,3 +1,4 @@
 1838. this question is solve by the my probolem with the givven test case but not in the othercase.
 31. Next purmutation. 
 46. purmutation.
+inpliment stack using stack que.
---
+++ Playlist/Striver/StackUsinLinkedList/stackUsinLInkedlIst.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyStack {
+private:
+    struct Node {
+        int data;
+        Node* next;
+        Node(int val) : data(val), next(nullptr) {}
+    };
+
+    Node* head;   // top of the stack
+
+public:
+    MyStack() {
+        head = nullptr;
+    }
+
+    void push(int x) {
+        Node* newNode = new Node(x);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    int pop() {
+        if (head == nullptr) {
+            return -1;  // stack underflow
+        }
+
+        int val = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return val;
+    }
+
+    int top() {
+        if (head == nullptr) {
+            return -1;
+        }
+        return head->data;
+    }
+
+    bool empty() {
+        return head == nullptr;
+    }
+
+    ~MyStack() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
