b66fdbde96f10c7bae450833b391f7b18ba772ed
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-28 14:35:18 +0530

    20 valid parenthesis

---
+++ Leetcode/easy/20ValidParenthesis/validParenthesis.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+
+        for(char ch:s){
+
+            if(ch == ')'){
+                if(!stack.top() != ch){
+                    return false;
+                }else
+                    stack.pop();
+            }else if(ch == '}'){
+                if(!stack.top() !=  ch){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }else if(ch == ']'){
+                if(!stack.top() != ch){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }else{
+                stack.push(ch);
+            }
+
+        }
+
+        return stack.size() == 0;
+    }
+};
+
+int main() {
+    Solution sol;
+    string example = "(())";
+    bool result1 = sol.isValid(example);
+    if(result1) cout << "Working fine your algorithum"<< endl;
+
+    string example2= "(]";
+
+    bool result2  = sol.isValid(example2);
+
+    if(!result2) cout << "working algo"<< endl;
+
+    return 0;
+}
