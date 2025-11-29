0e6706c768010d0d58223c2962bc51232882d741
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-29 09:02:02 +0530

    daily completed

---
+++ Leetcode/medium/424LongestRepeatingCharacterReplacement/maximumlengthwhenreapeatincharacter.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacementBruteForce(string s, int k) {
+        /// maximum lenght
+        int maxLength = 0;
+
+        int left = 0 ;
+
+        ///
+        int charCout = 0;
+        char ch= s[0];
+        for(int right = 0 ; right < s.length() ; right++){
+
+            // if() {
+            //     break;
+            //     maxLength = max(maxLength , right - left + 1 );
+            // }
+
+            // if(s[right] != ch ){
+            // }
+            
+
+
+        }
+    }
+    int characterReplacement(string s, int k) {
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
