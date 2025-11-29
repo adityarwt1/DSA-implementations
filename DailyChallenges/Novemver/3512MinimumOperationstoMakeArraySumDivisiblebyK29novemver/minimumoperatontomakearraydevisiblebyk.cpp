ceb5c7b4a56d236b775edfe2f120284391175a6a
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-29 09:17:20 +0530

    minimum operatonto make make array sum divisible by k

--- DailyChallenges/Novemver/3512MinimumOperationstoMakeArraySumDivisiblebyK29novemver/minimumoperatontomakearraydevisiblebyk.cpp
+++ DailyChallenges/Novemver/3512MinimumOperationstoMakeArraySumDivisiblebyK29novemver/minimumoperatontomakearraydevisiblebyk.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 class Solution {
 public:
-    
-};
+    int minOperations(vector<int>& nums, int k) {
+        long long totalSum = nums[0];
 
+        for(int i = 1 ; i < nums.size() ; i++){
+          totalSum += nums[i];
+        }
+
+        return (int)totalSum% k;
+    }
+}; 
 int main() {
     Solution sol;
-    
+    vector<int> nums = {3,9,7};
+    int k = 5;
+    int result = sol.minOperations(nums , k);
+    cout << result << endl;
     return 0;
 }
