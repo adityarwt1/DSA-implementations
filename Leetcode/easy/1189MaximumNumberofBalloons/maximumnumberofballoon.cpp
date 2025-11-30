000ad8f758dfec3c07231024bc9b2e6d0a7562ad
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-11-30 19:16:57 +0530

    maximum number of balloon

--- Leetcode/easy/1189MaximumNumberofBalloons/maximumnumberofballoon.cpp
+++ Leetcode/easy/1189MaximumNumberofBalloons/maximumnumberofballoon.cpp
@@ -17,7 +17,7 @@ public:
         minBalloon = min(minBalloon,count['b' -'a']);
         minBalloon = min(minBalloon, count['a' - 'a']);
         minBalloon = min(minBalloon,count['l' -'a'] /2);
-        minBalloon = min(minBalloon, count['o' , 'a'] /2);
+        minBalloon = min(minBalloon, count['o' - 'a'] /2);
         minBalloon = min(minBalloon, count['n' -'a']);
 
         return minBalloon;
@@ -26,6 +26,11 @@ public:
 
 int main() {
     Solution sol;
-    
+    string example1 = "loonbalxballpoon";
+    string example2 = "nlaebolko";
+    int numberOfballoon1 = sol.maxNumberOfBalloons(example1);
+    int numberOfballoon2 = sol.maxNumberOfBalloons(example2);
+    cout << "number of balloon " << numberOfballoon1<< endl;
+    cout << "number of balloon " << numberOfballoon2<< endl;
     return 0;
 }
