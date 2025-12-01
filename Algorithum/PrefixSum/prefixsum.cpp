b58c742189947b46dee883a3c4a288e6f74e40db
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-01 19:17:04 +0530

    prefix sum is done

--- Algorithum/PrefixSum/prefixsum.cpp
+++ Algorithum/PrefixSum/prefixsum.cpp
@@ -3,15 +3,24 @@ using namespace std;
 
 class Solution {
 public:
+/**
+ * i) intution for the prefixSum 
+ * i) hamane observe kiya ki sum karate samay picchara sum include ho jata tha
+ * ii) new array(vector) banaya jisame index ke hisab se picchala sum rahata tha
+ * iii) ab picchala sum rahata tha to 1 se start karane ka yahi karan ban gaya
+ */
     vector<int> prifixSum(vector<int> nums){
-        vector<int> prefix(nums.size());
-        prefix[0] = nums[0];
+        vector<int> prefixSum(nums.size());
 
-        for(int i =  1; i < nums.size() ; i++){
-            prefix[i] = prefix[i-1] + nums[i];
+        // access the previous so first asign the value
+        prefixSum[0] = nums[0];
+
+        for(int i  = 1 ; i <  nums.size() ; i++){
+
+            prefixSum[i] = prefixSum[i-1] + nums[i];
         }
 
-        return prefix;
+        return prefixSum;
     } 
     
     
