8c6fb261417381b208b02d832f4ef3669772ee6d
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-02 10:46:31 +0530

    maximum length

---
+++ RoadMap/PrefixSum/maximumLengthsum/maximumlength.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums , int k ){
+
+        // scence check karane ke liye harame pass
+        unordered_map<int , int> map;
+
+        map[0] = -1;// from the start
+
+        int prefixSum = 0 ;
+        int maxLength =0;
+
+        // lenght ke liye i se start karan ahai
+    
+
+        for(int i =  0 ; i < nums.size() ;i++){
+
+            // prefixsum = 
+
+            prefixSum += nums[i];
+
+        /// wehre shoud he nutralize this
+        int remainder = prefixSum -k;
+
+        // finding the scene value
+        if(map.find(remainder) != map.end()){
+            maxLength = max( i - map[remainder ] ,maxLength );
+            
+        }
+
+        // agar nahi hai to is ko stor karo
+        if(map.find(remainder) == map.end()){
+            map[prefixSum] = i;
+        }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
