3b1702a0ae2e694264cb4d7cf9cc56d8fa5525bb
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-03 20:44:05 +0530

    done the middle fo the linked list

---
+++ RoadMap/LinkedList/easy/876MiddleoftheLinkedList/middleoflinkedlist.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ struct ListNode {
+     int val;
+    ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode* temp = head;
+        ListNode* slow = temp;
+        ListNode* fast = temp;
+
+        while(fast && fast->next){
+          slow = slow->next;
+          fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
