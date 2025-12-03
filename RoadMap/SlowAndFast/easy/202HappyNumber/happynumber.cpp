1bbf752d73f0aa032048d7b75929b2bd58d8d975
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-03 21:37:35 +0530

    happy number

---
+++ RoadMap/SlowAndFast/easy/202HappyNumber/happynumber.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getNext(int n){
+        int sum = 0;
+
+        while(n >0){
+
+            int remainder = n % 10;
+            sum += remainder * remainder;
+
+            n/= 10;
+        }
+
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getNext(n);
+
+        while(slow != fast && fast != 1){
+
+            slow = getNext(slow);
+            fast = getNext(getNext(slow));
+        }
+
+        return fast == 1;
+    }
+};
+int main() {
+    Solution sol;
+    
+    return 0;
+}
