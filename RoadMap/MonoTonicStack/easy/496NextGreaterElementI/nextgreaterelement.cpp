366d9fc5cdb6dfd7a77b5fb8171a7d41cb0c7033
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-04 14:09:43 +0530

    next greater element is done

--- RoadMap/MonoTonicStack/easy/496NextGreaterElementI/nextgreaterelement.cpp
+++ RoadMap/MonoTonicStack/easy/496NextGreaterElementI/nextgreaterelement.cpp
@@ -4,35 +4,50 @@ using namespace std;
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-     int n = nums1.size();
-     
-     vector<int> ans(n, -1);
+        // storing the greate element for tghe nums21 vector
+         unordered_map<int ,int> nge;
 
-     stack<int> stack;
-
-     for(int i   = 0 ; i < n ; i++){
+         // when at the end of the stack have the greater element
+         stack<int> st;
 
 
-        // if the last element greate thant return the value4
-        while(!stack.empty() && nums1[i] > nums1[stack.top()]){
-            ans[stack.top()] = nums1[i];
-            stack.pop();
-        }
+         // nums2 for the comparigng the nums1 value
+         for(int num :nums2){
 
-        // otherwise push the current index to it
+            // if the currren value larger so tha tthe next value is also the larger
+            while(!st.empty() && num > st.top()){
+                nge[st.top()] = num;
+                st.pop();
+            }
+            // starogint the value
+            st.push(num);
+         }
+         
+         // remainng element have no enxt greter elemen
+         while(!st.empty()){
+            nge[st.top()] = -1;
+            st.pop();
+         }
 
-        stack.push(i);
-        
-     }
+         vector<int> ans;
+         for(int num:nums1){
+            ans.push_back(nge[num]);
+         }
 
-     return ans;
-     
+         return ans;
     }
 
 
 };
 int main() {
     Solution sol;
-    
+
     return 0;
 }
+/// notte by own
+/// hame num2 ke compition me nikalana ha isliye num to ka use karen
+// start current eleme bada stack ke top se to top ke liye vahi great emelent hoga
+
+// at the end of the agar eleme exist karata hai to vahi return kar dena hai
+
+/// num nki value check karenge kya exist karenge
