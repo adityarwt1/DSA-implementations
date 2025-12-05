87d1a16dd05d0952680522bf7c839f82f60cb9e5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-05 17:03:21 +0530

    merge interevels

--- RoadMap/Intervals/medium/56MergeIntervals/mergeintervels.cpp
+++ RoadMap/Intervals/medium/56MergeIntervals/mergeintervels.cpp
@@ -4,21 +4,27 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //
         vector<vector<int>> merged;
 
+        /// sortin 
         sort(intervals.begin() , intervals.end());
 
+        // for the comparision pussh in the first timew
         merged.push_back(intervals[0]);
 
+        //start fornm the 
         for(int i =1 ; i < intervals.size() ; i++){
-
+            // compare from the old
             vector<int>& prev = merged.back();
             vector<int>& current = intervals[i];
 
-
+            // if the start of the current is lessed that end of previous 
             if(current[0] <= prev[1] ){
+                // replace itht new max if it is
                 prev[1] = max(current[1] , prev[1]);
             }else{
+                // otherwise simply push them to the 
                 merged.push_back(intervals[i]);
             }
         }
@@ -31,6 +37,6 @@ int main() {
     Solution sol;
     vector<vector<int>> intervels  = {{1,3},{2,6},{8,10},{15,18}};
     vector<vector<int>> output = sol.merge(intervels);
-    
+
     return 0;
 }
