28f70f4cb6803e488fdc5e0ff91c52768cb0b2a4
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-06 09:35:50 +0530

    binary seearch is done

---
+++ RoadMap/BinarrySearch/BinarySearchBaseForumula.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        /**
+         * exmaple :-  [-1,0,3,5,9,12] , target = 9 
+         * high = nums.size() -1 = 6 -1
+         * 
+         * while(low<= high) 
+         * i) 
+         * mid = 0+5 /2  = 2 
+         * 
+         * nums[2] < target = 3 < 9 to low = 2 +1 = 3
+         * 
+         * ii)binary search
+        * (3 +5 ) /2 = 4 
+        * nums[4] ==  target = 9 == 9 return mid (4);
+         */
+        int low = 0 , high = nums.size() -1;
+
+        while(low <= high){
+
+            int mid = (low + high) /2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                low = mid +1;
+            }else{
+                high = mid -1;
+            }
+            
+        }
+
+        return -1;
+    }
+};
+int main() {
+    Solution sol;
+    
+    return 0;
+}
