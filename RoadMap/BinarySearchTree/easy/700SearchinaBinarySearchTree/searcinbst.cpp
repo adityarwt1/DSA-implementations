fb376dd39214cb20890e405d278bac562109868d
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-07 10:11:43 +0530

    search in the bst is doen

---
+++ RoadMap/BinarySearchTree/easy/700SearchinaBinarySearchTree/searcinbst.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        // if not found the in the operation 
+        if(!root) return nullptr;
+
+        // if value found
+        if(val == root->val) return root;
+
+        if(val < root->val) return searchBST(root->left, val);
+
+        return searchBST(root->right ,val);
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
