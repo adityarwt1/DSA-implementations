dd9b1095aaa08f07b23da855d98da448bb86a117
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-09 10:09:09 +0530

    bfs template is done

---
+++ RoadMap/BFS(BreathFirstSearch)/template.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void BFS(int start , vector<vector<int>>& graph){
+        /// start in the starrting point and given graph is the starting point for this graph
+        queue<int> q;
+        // jo pahale mila usako pahale process karo
+
+        vector<int> visited(graph.size() , 0); // 0 means not visitef one mens visited
+
+        q.push(start);
+        /// [1,2,3]
+        visited[start]= 1;
+
+        while(!q.empty()){
+            int node = q.front(); /// taing the front 
+            q.pop();
+
+            for(int nei :graph[node]){
+                // visiting the neighjboour if not exit ini the visited
+                if(!visited[nei]){
+                    // this wiull marked
+                    visited[nei] =1;
+                    // push neighbour into que
+                    q.push(nei);
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    return 0;
+}
