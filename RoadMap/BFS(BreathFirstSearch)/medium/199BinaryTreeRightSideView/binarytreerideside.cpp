a1b45d70a2047966dea675ed15d30454eec2af79
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-09 13:26:06 +0530

    binary tree side cpp

---
+++ RoadMap/BFS(BreathFirstSearch)/medium/199BinaryTreeRightSideView/binarytreerideside.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+      int val;
+     TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            TreeNode* node = nullptr;
+
+            while (size--) {
+                node = q.front();
+                q.pop();
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            // node now stores the last node of this level
+            result.push_back(node->val);
+        }
+
+        return result;
+    }
+};
