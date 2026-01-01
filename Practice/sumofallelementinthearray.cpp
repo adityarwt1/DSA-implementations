082d569675286e4ce7619a784b1d6f10bf5caf16
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-01-01 15:47:40 +0530

    simple example added

---
+++ Practice/sumofallelementinthearray.cpp
@@ -0,0 +1,20 @@
+//
+// Created by adity on 01-01-2026.
+//
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    static int sumOfNumber(int a , int b) {
+        return a+b;
+    }
+};
+int main() {
+    Solution s;
+    cout << Solution::sumOfNumber(2,3) << endl;
+    return 0;
+
+}
