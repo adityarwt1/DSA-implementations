242f9794bb5ccc57e377029a31de64e09bcf701f
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-02-21 21:33:01 +0530

    21 feb work is done now

---
+++ DSARestart/dailyOne/february/21FebcheckIfArrrayIsSorted.ts
@@ -0,0 +1,25 @@
+// checkinngg the arrray is sorted by checkin the dip 
+/// mean when the upcomming the numbber is nott less the previous one mean that the array is sored already 
+
+const testArrray = [1,2,3,4,5,6,7,8,9]
+const testArray2 = [1,2,3,4,5,6,4]
+
+// write the check funtion who detect the dip of the given arrray whwer the dip is actully exist 
+
+const checkArrayIsSorted = (numsArray:number[])=>{
+    // checking throught thhe running on vala loop 
+    for(let i = 0 ; i < numsArray.length -1 ; i++ ){
+        if(numsArray[i] > numsArray[i+1]){
+            console.log("iff condition is worked at the ", numsArray[i], numsArray[i+1])
+            return false
+        }
+    }
+    // difing the default retur value
+    return true
+}
+
+// checking the sorted array 
+const isSorted1 = checkArrayIsSorted(testArrray)
+console.log(isSorted1 ? "Array is sorted as the program:":"array is not sorted!")
+const isSorted2 = checkArrayIsSorted(testArray2)
+console.log(isSorted2 ? "this return true ":"array is not sorted!")
